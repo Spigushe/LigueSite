a07c63d6ca6fbd695a793ceba6242ff45f5ec266
Spigushe <martin.cuchet@gmail.com>
2020-06-14 20:01:18 +0200

    Update Controleur.php

--- Afficher/Controleur.php
+++ Afficher/Controleur.php
@@ -6,7 +6,7 @@ if (isset($_GET['saison']) && isset($_GET['ligue'])) {
 	require_once("Afficher/Resultats_globaux.php");
 }
 
-function getContenu () {
+function getContenu ($resultats) {
 	if (isset($_GET['saison']) && isset($_GET['ligue'])) {
 		if ($_GET['ligue'] == 'Placement') { // Résumé placements
 			require_once('Afficher/Elements/Placement.php');
@@ -15,7 +15,7 @@ function getContenu () {
 		} else require_once('Afficher/Elements/Ligue.php'); // Résumé Ligue
 	}
 	require_once('Afficher/Elements/Saison.php'); // Page d'accueil
-	return Affichage($parties);
+	return Affichage($resultats);
 }
 
 require_once('Afficher/Vue.php');
