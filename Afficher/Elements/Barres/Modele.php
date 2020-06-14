89f6bb292680b87196b1190a6c6a47b321274b13
Spigushe <martin.cuchet@gmail.com>
2020-06-14 20:01:40 +0200

    Update Modele.php

--- Afficher/Elements/Barres/Modele.php
+++ Afficher/Elements/Barres/Modele.php
@@ -1,68 +1,17 @@
 <?php
-function getListeDecks () {
-	$saison = $_GET['saison'];
-	$ligue  = $_GET['ligue'];
-
-	$sql =	"SELECT * " . //
-			"FROM ligues " . //
-			"WHERE " . //
-				"num_saison = '$saison' " . //
-			"AND " . //
-				"nom_ligue LIKE '%$ligue%';";
-
-	// Liste des decks
-	return executerRequete($sql)->fetch()['liste_decks'];
-}
-
-function getTableDecks () {
-	return preg_split("/, /",getListeDecks());
-}
-
-function isMatchPlayed ($id1, $id2) {
-	if (!isset($_GET['saison']) && ($_GET['saison'] != "")) {
-		$resultats = "resultats";
-	} else if (SaisonEnCours() != $_GET['saison']) {
-		$resultats = "resultats_saison".$_GET['saison'];
-	} else {
-		$resultats = "resultats";
-	}
-
-	$sql =	"SELECT " . //
-				"COUNT(id_resultat) " . //
-			"FROM " . //
-				"$resultats " . //
-			"WHERE " . //
-				"(id_deck1 = '$id1' AND id_deck2 = '$id2') " . //
-				"OR " . //
-				"(id_deck1 = '$id2' AND id_deck2 = '$id1') " . //
-			"ORDER BY " . //
-				"$resultats.id_resultat " . //
-			"DESC;";
-
-	return executerRequete($sql)->fetchColumn();
-}
-
-function getMatchesPlayed () {
-	$tableDecks = getTableDecks();
-
+function getMatchesPlayed ($helper) {
 	$count = 0;
-	for ($i = 0; $i < count($tableDecks); $i++) {
-		for ($j = 0; $j < $i; $j++) {
-			$count += isMatchPlayed($tableDecks[$i],$tableDecks[$j]);
+
+	foreach (array_keys($helper) as $deck1) {
+		foreach (array_keys($helper[$deck1]) as $deck2) {
+			$count += count($helper[$deck1][$deck2]);
 		}
 	}
 
-	return $count;
+	return $count/2;
 }
 
-function getMaxMatches () {
-	$tableDecks = getTableDecks();
-
-	// Somme de i à n = n * (n+1) / 2
-	// Il y a (n-1) matches au plus
-	return count($tableDecks)*(count($tableDecks)-1)/2;
-}
-
-function getAvancement () {
-	return round( getMatchesPlayed() / getMaxMatches() * 100 , 0);
+function getMaxMatches ($helper) {
+	// Somme de 1 à n = n * (n+1) / 2
+	return count($helper)*(count($helper)-1)/2;
 }
