600585ec776fdf9f1b8c5c8384ac2625ca631b8b
Spigushe <martin.cuchet@gmail.com>
2020-06-15 22:26:27 +0200

    Update Ligue.php

--- Afficher/Elements/Ligue.php
+++ Afficher/Elements/Ligue.php
@@ -1,35 +1,20 @@
 <?php
-function Affichage ( $saison = "" , $ligue = "" ) {
-	$infos = getInformationsDansSaisonEtLigue($saison,$ligue);
-	$resultats = getResultatsDansSaisonEtLigue($saison,$ligue);
+// Fonctions annexes
+require_once('Afficher/Elements/Modele.php');
+require_once('Afficher/Elements/Ligue/Modele.php');
+// Helper
+require_once("Afficher/Resultats.php");
 
+function Affichage ($helper) {
 	$affichage  = "<div class='my-5'></div>";
-	$affichage .= progressLigue($saison, $ligue);
+	$affichage .= progressLigue($helper);
 	$affichage .= "<div class='my-5'></div>";
 	$affichage .= "<h3 class='mt-3'>Résultats et informations</h3>";
-	$affichage .= tableauJoueurs ($infos, $resultats);
+	$affichage .= tableauJoueurs($helper);
 	$affichage .= "<div class='my-5'></div>";
 	$affichage .= "<h3 class='mt-3'>Résultat des parties</h3>";
-	//$affichage .= "<p class='mb-2'><i>Il se peut qu'il faille faire défilier horizontalement le tableau pour accéder à l'ensemble des informations</i></p>";
-	$affichage .= tableauResultats ($infos, $resultats);
+	$affichage .= "<p class='mb-2'><i>You might have to scroll horizontally to access everything</i></p>";
+	$affichage .= tableauResultats ($helper);
 
 	return $affichage;
-}
-
-function tableauJoueurs ($infos, $resultats) {
-	ob_start();
-	require_once('Afficher/Elements/Tableaux/TableauJoueurs.php');
-	return ob_get_clean();
-}
-
-function tableauResultats ($infos, $resultats) {
-	ob_start();
-	require_once('Afficher/Elements/Tableaux/TableauResultats.php');
-	return ob_get_clean();
-}
-
-function progressLigue ($saison , $ligue) {
-	ob_start();
-	require_once('Afficher/Elements/Barres/BarreAvancement.php');
-	return ob_get_clean();
 }
