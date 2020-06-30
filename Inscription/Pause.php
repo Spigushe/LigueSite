9b8f199df4b49b7546e9c2698885e52018ddc0a6
Spigushe <martin.cuchet@gmail.com>
2020-06-30 11:57:44 +0200

    renaming

---
+++ Inscription/Pause.php
@@ -0,0 +1,13 @@
+<?php
+
+function Action ($params)
+{
+	// On vérifie que l'utilisateur est déjà inscrit
+	if (dejaInscrit($params['id']) == 'XXX') {
+		return "Erreur 000 : Utilisateur non inscrit";
+	}
+
+	$pseudo = getPseudo($params['id']);
+	$ligue = getLigue($params['id']);
+
+	return "$pseudo--$ligue";
