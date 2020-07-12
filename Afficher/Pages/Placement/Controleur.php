3e9b063a9c2fd5cbdf61d3e58d560e51a50828f2
Spigushe <martin.cuchet@gmail.com>
2020-07-12 14:07:23 +0200

    PLACEMENT: Harmonisation

--- Afficher/Pages/Placement/Controleur.php
+++ Afficher/Pages/Placement/Controleur.php
@@ -1,21 +1,8 @@
 <?php
 // Fonctions annexes
 require_once('Afficher/Elements/Modele.php');
+
+// Affichage des Placements
 require_once('Afficher/Pages/Placement/Modele.php');
-
-
-if (preg_match("/[0-9]/",$_GET['ligue'])) {
-	// Si on veut afficher un groupe de Placement
-
-	// On utilise le helper de la ligue
-	require_once("Afficher/Elements/Resultats/Details.php");
-
-	// Vue
-	require_once("Afficher/Elements/Vue/Vue_Groupe.php");
-} else {
-	// Helper
-	require_once("Afficher/Elements/Resultats/Placement.php");
-
-	// Vue
-	require_once("Afficher/Elements/Vue/Vue_Global.php");
-}
+require_once('Afficher/Pages/Placement/Helper.php');
+require_once('Afficher/Pages/Placement/Vue_Playoff.php');
---
+++ Afficher/Pages/Placement/Vue_Placement.php
@@ -0,0 +1,7 @@
+<?php
+// Affichage selon niveau de profondeur
+if (preg_match("/-/",$_GET['ligue'])) {
+	require_once("Afficher/Elements/Vue/Vue_Poule.php");
+} else {
+	require_once("Afficher/Elements/Vue/Vue_Placement.php");
+}
