a720a671816f880287b9fbee152d991c5483720a
Spigushe <martin.cuchet@gmail.com>
2020-07-12 14:08:07 +0200

    SAISON: Harmonisation

--- Afficher/Pages/Saison/Controleur.php
+++ Afficher/Pages/Saison/Controleur.php
@@ -1,10 +1,8 @@
 <?php
 // Fonctions annexes
 require_once('Afficher/Elements/Modele.php');
+
+// Affichage de la Saison
 require_once('Afficher/Pages/Saison/Modele.php');
-
-// Helper
 require_once("Afficher/Pages/Saison/Helper.php");
-
-// Vue
-require_once("Afficher/Elements/Vue/Vue_Global.php");
+require_once("Afficher/Pages/Saison/Vue_Saison.php");
