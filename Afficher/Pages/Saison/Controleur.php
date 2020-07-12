59ab10fe968e23e571c8c1de4423384247c9ce17
Spigushe <martin.cuchet@gmail.com>
2020-07-12 13:54:20 +0200

    SEASON HELPER: Clarification

--- Afficher/Pages/Saison/Controleur.php
+++ Afficher/Pages/Saison/Controleur.php
@@ -4,7 +4,7 @@ require_once('Afficher/Elements/Modele.php');
 require_once('Afficher/Pages/Saison/Modele.php');
 
 // Helper
-require_once("Afficher/Elements/Resultats/Saison.php");
+require_once("Afficher/Pages/Saison/Helper.php");
 
 // Vue
 require_once("Afficher/Elements/Vue/Vue_Global.php");
