7d6f87a87cf4ac3cf8e0d78b7a6645b12af03aab
Spigushe <martin.cuchet@gmail.com>
2020-07-19 12:25:18 +0200

    BASE: Update

--- Modele.php
+++ Modele.php
@@ -7,12 +7,6 @@ function getBdd() {
 	$bdd = new PDO("sqlite:BD/database.sqlite");
 	    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 	    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	// Vérification de l'existence des tables
-	$bdd->query($_SESSION['dicoTables']['tableParticipants']);
-	$bdd->query($_SESSION['dicoTables']['tableRoles']);
-	$bdd->query($_SESSION['dicoTables']['tableDecks']);
-	$bdd->query($_SESSION['dicoTables']['tableResultats']);
-	$bdd->query($_SESSION['dicoTables']['tableLigues']);
 	// On passe la connexion à la base de données
 	return $bdd;
 }
@@ -20,15 +14,15 @@ function getBdd() {
 function executerRequete($sql, $params = null) {
 	if ($params == null) {
 		$resultat = getBdd()->query($sql);   // exécution directe
-	} else {
-		$resultat = getBdd()->prepare($sql); // requête préparée
-		$resultat->execute($params);
+		return $resultat;
 	}
+	
+	$resultat = getBdd()->prepare($sql); // requête préparée
+	$resultat->execute($params);
 	return $resultat;
 }
 
-function file_get_contents_utf8($fn) {
-     $content = file_get_contents($fn);
-      return mb_convert_encoding($content, 'UTF-8',
-          mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true));
+function file_get_contents_utf8($file) {
+	$content = file_get_contents($file);
+	return mb_convert_encoding($content, 'UTF-8', mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true));
 }
