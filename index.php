1bb78418db2872d671ec6e86160da51ba9b0f9ae
Spigushe <martin.cuchet@gmail.com>
2020-07-19 13:14:28 +0200

    INDEX: Contantes fix

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ define('__MAX_MATCHES__', 1);
 $parametres = array_merge($_GET,$_POST);
 
 // Numéro de la saison en cours
-define('__SAISON__', (!isset($parametres['saison']) || ($parametres['saison'] == "")) ? 3 : $_GET['saison'] * 1);
+define('__SAISON__', (!isset($parametres['saison']) || ($parametres['saison'] == "")) ? 3 : $parametres['saison'] * 1);
 // Ligue en cours
-define('__LIGUE__', stripslashes($parametres['ligue']));
+define('__LIGUE__', $parametres['ligue']);
 
 /* ROUTAGE DE LA REQUETE */
 require_once 'Controleur.php';
